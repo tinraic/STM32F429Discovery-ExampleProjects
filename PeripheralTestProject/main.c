27df196157a5a19e1420c8309890bd753294dbaf
Tin Raic <tin.raic@fer.hr>
2017-11-14 11:51:24 +0100

    Added PWM test project

--- PeripheralTestProject/main.c
+++ PeripheralTestProject/main.c
@@ -1,39 +1,96 @@
-#include "main.h"
+#include "stm32f4xx.h"
 
-
-GPIO_InitTypeDef GPIO_InitStructure;
 
 static __IO uint32_t TimingDelay;
 
+TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
+TIM_OCInitTypeDef  TIM_OCInitStructure;
 
-static void Delay(__IO uint32_t nTime);   
+
+uint16_t CCR1_Val = 1400;
+uint16_t CCR2_Val = 262;
+uint16_t CCR3_Val = 175;
+uint16_t CCR4_Val = 87;
+uint16_t PrescalerValue = 0;
+
+
+void TIM_Config(void);
 
 
 int main(void)
 {
+	int i=0;
 
-  STM_EVAL_LEDInit(LED3);
-	
-  STM_EVAL_LEDInit(LED4);
+  TIM_Config();
+  
+  RCC_TIMCLKPresConfig(RCC_TIMPrescActivated);
 
-  STM_EVAL_LEDOn(LED3);
+  /* Compute the prescaler value */
+  PrescalerValue = (uint16_t) (SystemCoreClock / 21000000) - 1;
 
-  if (SysTick_Config(SystemCoreClock / 1000))
-  { 
-    while (1);
-  }
+  /* Time base configuration */
+  TIM_TimeBaseStructure.TIM_Period = 699;
+  TIM_TimeBaseStructure.TIM_Prescaler = PrescalerValue;
+  TIM_TimeBaseStructure.TIM_ClockDivision = 0;
+  TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+
+  TIM_TimeBaseInit(TIM4, &TIM_TimeBaseStructure);
+
+  /* PWM1 Mode configuration: Channel1 */
+  TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+  TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+  TIM_OCInitStructure.TIM_Pulse = CCR1_Val;
+  TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+
+  TIM_OC1Init(TIM4, &TIM_OCInitStructure);
+
+  TIM_OC1PreloadConfig(TIM4, TIM_OCPreload_Enable);
+
+  TIM_ARRPreloadConfig(TIM4, ENABLE);
+
+  /* TIM4 enable counter */
+  TIM_Cmd(TIM4, ENABLE);
 
   while (1)
   {
+		for(i=0; i< 1000000; i++);
+		
+			if(CCR1_Val > 0)
+			{
+				CCR1_Val -= 50;
+				TIM_OCInitStructure.TIM_Pulse = CCR1_Val;
+				TIM_OC1Init(TIM4, &TIM_OCInitStructure);
+			}
+			else 
+      {
+				CCR1_Val = 1400;
+				TIM_OCInitStructure.TIM_Pulse = CCR1_Val;
+				TIM_OC1Init(TIM4, &TIM_OCInitStructure);
+			}
+		
+	}
+}
 
-    STM_EVAL_LEDToggle(LED4);
+void TIM_Config(void)
+{
+  GPIO_InitTypeDef GPIO_InitStructure;
 
-    Delay(2500);
+  /* TIM4 clock enable */
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
 
-    STM_EVAL_LEDToggle(LED3);
+  /* GPIOD clock enable */
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+  
+  /* GPIOD Configuration: TIM4 CH1 (PD12), TIM4 CH2 (PD13), TIM4 CH3 (PD14) and TIM4 CH4 (PD15) */
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP ;
+  GPIO_Init(GPIOB, &GPIO_InitStructure); 
 
-    Delay(1000);
-  }
+  /* Connect TIM4 pins to AF2 */  
+  GPIO_PinAFConfig(GPIOB, GPIO_PinSource6, GPIO_AF_TIM4);	
 }
 
 void Delay(__IO uint32_t nTime)
@@ -50,3 +107,5 @@ void TimingDelay_Decrement(void)
     TimingDelay--;
   }
 }
+
+
